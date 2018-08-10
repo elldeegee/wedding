1942431e190b9781c8c0426ab7831c7068ee1167
elldeegee <lauren.glasky@gmail.com>
2018-08-09 21:54:06 -0400

    new live version

--- Template/accommodations/index.php
+++ Template/accommodations/index.php
@@ -5,8 +5,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wedding Accomodations</title>
+    <title>Wedding Details</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115975976-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-115975976-1');
+    </script>
 
     <!-- Library CSS -->
     <link href="../css/wedding_library.css" rel="stylesheet">
@@ -20,14 +29,14 @@
     <link rel="stylesheet" href="../css/leaves.css" type="text/css" media="screen">
 
     <!-- Google Fonts -->
-   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet">
-   <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
 
-</head>
-<body>
+    </head>
+    <body>
 
-  <!-- Page -->
-  <div class="wed_page" id="wed_page">
+    <!-- Page -->
+    <div class="wed_page" id="wed_page">
 
     <!-- Header -->
     <header>
@@ -58,7 +67,7 @@
 
 
     <!-- PAGE TITLE SMALL -->
-    <div class="wed_page_title wed_page_title_great wed_image_bck wed_fixed wed_wht_txt" data-stellar-background-ratio="0.2" data-image="../images/personal/dipping.jpg">
+    <div class="wed_page_title wed_page_title_great wed_image_bck wed_fixed wed_wht_txt" data-stellar-background-ratio="0.2" data-image="../images/personal/dipping_details.jpg">
 
 
       <!-- Over -->
@@ -69,8 +78,7 @@
         <div class="row">
 
           <div class="col-md-8">
-            <h1 class="wed_h1_title">Accommodations</h1>
-            <h4>Since our wedding is taking place during the height of the summer season, we strongly advise booking accommodations as soon as possible.<h4>
+            <h1 class="wed_h1_title">Wedding Details</h1>
           </div>
 
         </div>
@@ -88,56 +96,94 @@
         <div class="container text-left">
           <!-- Item -->
           <div class="row wed_row_padding">
-            <div class="col-md-offset-1 col-md-5">
+            <div class="col-md-offset-1">
               <div>
-                <h3>Residence Inn</h3>
-                <span>Riverhead, NY<br><br></span>
-                <p>
-                  A block of rooms has been reserved for our guests at a discounted rate. Please reference the Glasky / Silberman wedding when making your reservations by calling (###) ###-####. <br> <br>
-                  Transportation will be provided to and from the wedding venue.
-                </p>
+                <h4><i>We will be getting married at the Oceanbleu in Westhampton Beach, NY! <br> <br> Below you will find some details regarding logistics. <br>
+                  Please note, since our wedding is taking place during the height of the summer season, we <b>strongly</b> advise booking accommodations as soon as possible. </i>
               </div>
             </div>
+          </div>
+        </div>
+    </section>
+                <!-- section -->
+                <section>
+                  <div class="container text-center wed_great_titles">
+                  <h2>Accommodations</h2>
+                </div>
+              </section>
+                  <section class="">
+                  <div class="container text-left">
+                    <!-- Item -->
+                      <div class="col-md-offset-1">
+                        <div>
+                  <p> A block of rooms at two adjacent hotels has been reserved for our guests at a discounted rate.
+                    These rooms <b> must be booked prior to October 30th </b>to obtain the rate, so please book as soon as possible.<br> <br>
+                    <b> Mariott Residence Inn, Riverhead NY</b> <br>
+                    To book online, <a href="http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Glasky-Silberman%20Wedding%20Room%20Block%5Eisprh%60gswgswa%7Cgswgswb%7Cgswgswc%60309.00-509.00%60USD%60false%606%607/3/19%607/5/19%605/12/19&app=resvlink&stop_mobi=yes" target="_blank"><b>CLICK HERE</b></a>. <br>
+                    To book via phone, call (631) 905-5811 and reference the Glasky / Silberman wedding when making your reservations. <br> <br>
+                    <b> Hilton Garden Inn, Riverhead NY</b> <br>
+                    To book online, <a href="http://group.hiltongardeninn.com/Glasky-Silberman-Wed" target="_blank"><b>CLICK HERE</b></a>. <br>
+                    To book via phone, call (631) 727-2733 and reference the Glasky / Silberman wedding when making your reservations. <br> <br>
+                    Transportation will be provided to and from the above hotels and the wedding venue. <br><br>
+                    <i> Alternatively, you can explore airbnb and VRBO for vacation rentals or these other hotels: <br>
+                    -  <a href="https://www.hamptonoceanresort.com/" target="_blank"><b>Hampton Ocean Resort</b></a>, East Quogue NY (2 night min) <br>
+                    -  <a href="https://www.jedediahhawkinsinn.com/" target="_blank"><b>Jedediahh Hawkins Inn</b></a>, Jamesport NY <br>
+                    -  <a href="https://hamptonmaid.com/" target="_blank"><b>The Hampton Maid</b></a>, Hampton Bays NY (3 night min) <br>
+                    -  <a href="https://quogueclub.com/" target="_blank"><b>The Quogue Club</b></a>, Quogue NY (3 night min)</i> <br>
+                  </p>
+                  </div>
+                  <!-- Row End -->
+                </div>
+                <!-- Container End -->
+              </section>
+            <!-- Section end -->
 
-            <div class="col-md-5">
-              <img src="../images/personal/shackfun.jpg" class="fullwidth" alt="">
+            <!-- section -->
+            <section>
+              <div class="container text-center wed_great_titles">
+              <h2>Map</h2>
             </div>
-          </div>
-          <!-- Row End -->
+            </section>
+              <section class="">
+              <div class="container text-left">
+                <!-- map -->
+              <div id="map" class="wed_map col-md-6 wed_inside_map">
+                <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1kRgAHhwxwk5RWJ1HvpR3iyncjSLkf8DO" width="640" height="480"></iframe>
+              </div>
+              <!-- map end -->
+              <!-- Row End -->
+            </div>
+            <!-- Container End -->
+            </section>
+            <!-- Section end -->
 
-          </div>
-          <!-- container end -->
-
-
-        </section>
-        <!-- section end -->
-
+    <!-- section -->
+      <section>
+        <div class="container text-center wed_row_padding wed_great_titles">
+        <h2><br>Things to Do</h2>
+      </div>
+    </section>
+        <section class="">
+        <div class="container text-left">
+          <!-- Item -->
+            <div class="col-md-offset-1">
+              <div>
+        <p>
+          - <b>  Golf: </b> There are several courses in the area. Whether you're looking to do 9 or 18 holes, or to hit the range, we recommend <a href="http://www.cherrycreeklinks.com/" target="_blank"><b>Cherry Creek</b></a>. <br>
+          - <b>  Wine tasting: </b> Eastern Long Island is home to a number of great vineyards, which you can find <a href="http://www.liwines.com/wineries/" target="_blank"><b>HERE</b></a>. <br>
+          - <b>  Shopping: </b> In Riverhead you can find Tanger Outlet Mall as well as many of the big box retailers.<br>
+          - <b>  Walking around: </b> If you're looking to check out the local towns, head to Southampton. <br>
+          - <b>  The Riverhead Acquarium: </b> This acquarium is among the best in the US. <br>
+          - <b>  Splish Splash: </b> Riverhead is home to "Long Island's Best Waterpark." <br>
+          - <b>  Watersports: </b> SUP or Kayak in the Peconic with <a href="https://peconicpaddler.com/" target="_blank"><b>Peconic Paddler</b></a>. <br>
+          - <b>  Ice Cream: </b> Stop by <a href="http://snowflakeicecream.com/" target="_blank"><b>Snowflake</b></a>, our favorite (and debatably world famous) ice cream shop that serves homemade scoops, including our "go-to" Peconic Swamp Thing. <br>  <br>
+        </p>
+        </div>
+        <!-- Row End -->
+      </div>
+      <!-- Container End -->
     </section>
     <!-- Section end -->
-
-
-  <!-- White border -->
-  <div class="wed_white_inner_border">
-    <!-- section -->
-    <section id="where" class="wed_section_inner wed_image_bck wed_wht_txt wed_fixed lgms_celebrate" data-stellar-background-ratio="0.2" data-image="">
-      <!-- Over -->
-      <div class="wed_over" data-color="#3336ff" data-opacity="1"></div>
-      <div class="wed_over" data-color="#3336ff" data-opacity="0" data-blend="screen"></div>
-
-      <div class="container">
-        <div class="row wed_auto_height wed_middle_titles">
-          <div class="col-md-12 wed_image_bck wed_fixed text-center" >
-              <h4>Alternatively, we encourage you to explore airbnb and VRBO for vacation rentals.</h4>
-          </div>
-          <!-- col end -->
-        </div>
-        <!-- row end -->
-        <!-- <span class="wed_countdown" data-year="2019" data-month="07" data-day="04"></span> -->
-      </div>
-      <!-- container end -->
-
-    </section>
-    <!-- section end -->
 
           <!-- To Top -->
         <a href="#wed_page" class="wed_top ti ti-angle-up wed_go"></a>
@@ -145,8 +191,8 @@
       <!-- White border end -->
     </section>
     <!-- content end -->
-  </div>
-  <!-- Page End -->
+    </div>
+    <!-- Page End -->
 
 
 
